4806ac8f0affc859baf2b1b7c09c8474687701b6
David Wippel <david@trigo.at>
2017-05-11 09:31:32 +0200

    breaking: changed import/no-extraneous-dependencies and no-unused-vars to warn. should improve dev workflow

--- index.js
+++ index.js
@@ -19,15 +19,18 @@ module.exports = {
 			ignoreUrls: true,
 			ignoreComments: false,
 		}],
-		'no-underscore-dangle': ['error', { allow: [
-			'_id',
-			'_index',
-			'_score',
-			'_shards',
-			'_source',
-			'_type',
-		], allowAfterThis: true, allowAfterSuper: true }],
-		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		'no-underscore-dangle': ['error', {
+			allow: [
+				'_id',
+				'_index',
+				'_score',
+				'_shards',
+				'_source',
+				'_type',
+			],
+			allowAfterThis: true,
+			allowAfterSuper: true }],
+		'import/no-extraneous-dependencies': ['warn', { devDependencies: true }],
 		'import/named': 2,
 		'mocha/no-exclusive-tests': 'error',
 		'no-restricted-syntax': [
@@ -37,5 +40,6 @@ module.exports = {
 			'WithStatement',
 		],
 		'no-await-in-loop': 0,
+		'no-unused-vars': ['warn', { vars: 'local', args: 'after-used', ignoreRestSiblings: true }],
 	},
 };
