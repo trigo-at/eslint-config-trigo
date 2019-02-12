99930ebb95c91bcd5845ecc01123cbe855c4c1dd
David Wippel <david@trigo.at>
2019-02-12 20:36:26 +0100

    remove postinstall step

--- postinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-case "$INIT_CWD" in *eslint-config-trigo)
-   echo Own Package. Exiting
-    exit 0
-esac
-
-(pushd '../..' && install-peerdeps eslint-config-trigo --dev --only-peers && popd)
